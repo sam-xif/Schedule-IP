1418a8144f45d83d785996091ea7f9162590e923
Ryan Goggins <ryangoggins@Ryans-MBP.andover.edu>
2017-10-01 15:40:01 -0400

    Processing csv

--- src/models.py
+++ src/models.py
@@ -17,7 +17,7 @@ Base = declarative_base()
 # Classes
 class Class(Base):
     __tablename__= 'class_'
-    
+
     ID = Column(Integer, primary_key=True)
     className = Column(String)
     classCode = Column(String)
@@ -25,32 +25,35 @@ class Class(Base):
     section = Column(Integer)
     room = Column(String)
     instructor = Column(String)
-    
+
     def __repr__(self):
         pass
-        
+
+# class CourseRequest(Base): just for clarity, give each student a CourseRequest object?
+
+
 class Student(Base):
     __tablename__ = 'student'
-    
+
     ID = Column(Integer, primary_key=True)
     name = Column(String)
     graduatingClass = Column(Integer)
     studentId = Column(String)
     sex = Column(String)
     cluster = Column(String)
-    
+
     def __repr__(self):
         return "Student<name={0}, _class={1}, studentId={2}, sex={3}>".format(self.name, self._class, self.studentId, self.sex);
 
-    
+
 class Schedule(Base):
     __tablename__ = 'schedule'
-    
+
     ID = Column(Integer, primary_key=True)
     student = Column(Integer)
     _class = Column(Integer)
-    
-    
+
+
 """
 DO NOT REMOVE THIS LINE
 KEEP IT AT THE BOTTOM OF THE FILE

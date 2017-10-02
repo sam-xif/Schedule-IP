0d582b1e201ff2f9855124eb708c5602cc345192
Samuel Xifaras <s.xifaras999@gmail.com>
2017-10-01 21:20:36 -0400

    Added --no-upgrade option to rebuild_db.bat

--- rebuild_db.bat
+++ rebuild_db.bat
@@ -5,15 +5,28 @@ rem        Assumes database files have .db extension
 rem Requires sed
 rem TODO: Remove requirement for sed by using the migrate command to generate a new management script
 
+setlocal enableDelayedExpansion
+
 rem Change any of these variables to reflect your environment
 set REPO=migrate_repo
 set EXT=db
 set MANAGE_SCRIPT=manage.py
 set DB_NAME=schedule.%EXT%
+set UPGRADE=1
 
-if not "%1"=="" (
-    set DB_NAME=%1
+rem Process command-line args
+:argloop
+set arg=%1
+if /i "%~1"=="--no-upgrade" (
+	set UPGRADE=0
 )
+if not "%arg:~0,2%"=="--" (
+	if not "%~1"=="" (
+		set DB_NAME=%1
+	)
+)
+shift
+if not "%~1"=="" goto argloop
 
 rem Create the empty database file
 type nul > %DB_NAME%
@@ -24,6 +37,11 @@ python %MANAGE_SCRIPT% version_control --url=sqlite:///%DB_NAME%
 rem update the manage.py script to manage the new database
 sed -i "s;sqlite:///.*\.%EXT%;sqlite:///%DB_NAME%;g" %MANAGE_SCRIPT%
 
-rem upgrades to the most recent schema
-python %MANAGE_SCRIPT% upgrade
+if "%UPGRADE%"=="1" (
+	rem upgrades to the most recent schema
+	python %MANAGE_SCRIPT% upgrade
+) else (
+	echo Not upgrading. Run the command `python %MANAGE_SCRIPT% upgrade` to upgrade the database.
+)
 
+endlocal

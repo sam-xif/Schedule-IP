1813ed9fb9e0357ce5c1465208c800e327f8dccb
Samuel Xifaras <s.xifaras999@gmail.com>
2017-10-24 22:58:32 -0400

    Added algoscheduler.py

---
+++ src/algoscheduler.py
@@ -0,0 +1,11 @@
+"""
+File containing implementations of scheduling algorithms.
+"""
+
+from src.scheduler import *
+
+class BipartiteScheduler(BasicScheduler):
+    """
+    Implementation of scheduling using a bipartite graph algorithm
+    """
+    pass
